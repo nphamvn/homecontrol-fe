1e4ced29da04895b726c258b1066a8baa7fc93ec
Nam Pham <pvnam95@hotmail.com>
2021-10-26 19:01:41 +0900

    Added authenticate service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { LightComponent } from './components/light/light.component';
 import { NotificationComponent } from './components/notification/notification.component';
 import { AppComponent } from './components/app/app.component';
+import { LoginComponent } from './components/login/login.component';
 @NgModule({
   declarations: [
     AppComponent,
     LightComponent,
     NotificationComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
